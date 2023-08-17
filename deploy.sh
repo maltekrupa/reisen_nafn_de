7a62651180c522fd691f802814d6256008d30d14
Malte Krupa <github-nqyroppg@nafn.de>
2023-08-17 10:34:41 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ find ./static -name ".DS_Store" -delete
 rm -rf public
 hugo --minify --config config.toml
 pushd public
-rsync -ravl --delete . nafn.de:/var/www/virtual/computer/interrail22.nafn.de/
+rsync -ravl --omit-dir-times --delete ./ staticsites.w0.m.nafn.de:/usr/local/www/interrail22
 popd
